7edc0d5cb088c91ece88701051115be5719e882c
shal-in <shalin.a.ahasan@gmail.com>
2023-09-21 23:14:14 +0100

    cleaner code, fully working game with edge cases considered. visual colours need to be fixed

--- static/js/local.js
+++ static/js/local.js
@@ -1,30 +1,24 @@
-let currentPlayer = 'X'
-let gameWinner;
+// themes
+var rootStyles = getComputedStyle(document.documentElement);
+const backgroundCol =  rootStyles.getPropertyValue('--background');
+const gridCol = rootStyles.getPropertyValue('--gridColour');
+const gridBGCol = rootStyles.getPropertyValue('--gridBackground');
+const gridPlayedCol = rootStyles.getPropertyValue('--gridPlayed');
+const XCol = rootStyles.getPropertyValue('--XColour');
+const OCol = rootStyles.getPropertyValue('--OColour');
 
-// add all html elements into a 9x9 array
-let largeGridEl = Array.from(document.querySelectorAll('.large-grid'))
-let fullGameEl = []
-for (let i=0; i<largeGridEl.length; i++) {
+
+// add all html elements
+let largeGridEl = Array.from(document.querySelectorAll('.large-grid'));
+let fullGameEl = [];
+for (let i=0; i<9; i++) {
     let smallGridEl = Array.from(largeGridEl[i].querySelectorAll('.small-grid'));
     fullGameEl.push(smallGridEl);
 }
+let largeGridTextEl = Array.from(document.querySelectorAll('.large-grid-text'));
 
-// -1=no need for event listener
-// 0=add event listener, 
-// 1=has event listener (remove), 
-// 2=grid played
-let eventListenerTracker = [
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-]
 
+// set up arrays for tracking game
 let fullGameArray = [
     ['','','','','','','','',''],
     ['','','','','','','','',''],
@@ -37,145 +31,158 @@ let fullGameArray = [
     ['','','','','','','','','']
 ]
 let largeGameArray = ['','','','','','','','','']
+let eventListenerTracker = [
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener'],
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener'],
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener'],
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener'],
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener'],
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener'],
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener'],
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener'],
+    ['no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener', 'no event listener']
+]
 
-handleEventListeners('add')
-setUpBoardColours()
 
+// initialise variables for game
+let currentPlayer = 'X';
+let gameWinner;
+let nextLargeGrid = 'none';
+
+
+// set up game
+handleEventListeners('add');
+
+
+// define functions
 function boxClicked(e) {
     // remove event listeners and update immediate move
-    const id = e.target.id
-    let largeGridPlayed = parseInt(id[0])
-    let smallGridPlayed = parseInt(id[1])
-    e.target.innerHTML = currentPlayer
-    fullGameArray[largeGridPlayed][smallGridPlayed] = currentPlayer
+    const id = e.target.id;
+    let largeGridPlayed = parseInt(id[0]);
+    let smallGridPlayed = parseInt(id[1]);
+
+    e.target.innerHTML = currentPlayer;
+    e.target.style.color = currentPlayerColour();
+    fullGameArray[largeGridPlayed][smallGridPlayed] = currentPlayer;
     handleEventListeners('remove');
-    currentPlayer = changeCurrentPlayer()
-
-    // update consequences to the game
-    checkGame()
 
 
-    // prepare for next move
-    let nextLargeGrid = smallGridPlayed;
-    if (largeGameArray[nextLargeGrid] !== '') {
-        for (let row=0; row<eventListenerTracker.length; row++) {
-            for (let col=0; col<eventListenerTracker[row].length; col++) {
-                if (eventListenerTracker[row][col] !== 2) {
-                    eventListenerTracker[row][col] = 0;
-                }
-            }
-        }
-    }
-    else {
-        for (let col=0; col<eventListenerTracker[nextLargeGrid].length; col++) {
-            if (eventListenerTracker[nextLargeGrid][col] === -1) {
-                eventListenerTracker[nextLargeGrid][col] = 0
-            }
-        }
-    }   
+    // check game
+    checkGame(largeGridPlayed)
+    nextLargeGrid = nextLargeGridFunction(smallGridPlayed)
+    currentPlayer = changeCurrentPlayer();
 
-    // check if game winner
-    if (gameWinner) {
-        console.log(`game winner: ${gameWinner}`)
-        handleEventListeners('remove')
-    }
-    else {
-    handleEventListeners('add') 
-    }
-
-    setUpBoardColours()     
+    // set up for next turn
+    // updateColours()
+    handleEventListeners('add');
 }
 
 function handleEventListeners(event) {
     if (event === 'add') {
-        for (let row=0; row<eventListenerTracker.length; row++) {
-            for (let col=0; col<eventListenerTracker.length; col++) {
-                if (eventListenerTracker[row][col] === 0) {
-                    fullGameEl[row][col].addEventListener('click', boxClicked)
-                    eventListenerTracker[row][col] = 1
+        if (nextLargeGrid === 'none') { // at the start of the game
+            for (let row=0; row<9; row++) {
+                for (let col=0; col<9; col++) {
+                    fullGameEl[row][col].addEventListener('click', boxClicked);
+                    eventListenerTracker[row][col] = 'has event listener';
                 }
             }
         }
-    }
 
-    else if (event === 'remove') {
-        for (let row=0; row<eventListenerTracker.length; row++) {
-            for (let col=0; col<eventListenerTracker.length; col++) {
-                if (eventListenerTracker[row][col] === 1) {
-                    fullGameEl[row][col].removeEventListener('click', boxClicked)
-                    eventListenerTracker[row][col] = -1
-                }
-                if (fullGameArray[row][col] !== '') {
-                    eventListenerTracker[row][col] = 2
-                }
-            }
-        }
-    }
-}
-
-function checkGame() {
-    console.log('check game')
-    winningCombos = [[0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]]
-    
-    for (let row=0; row<largeGameArray.length; row++) {
-        if (largeGameArray[row] === '') {
-            let smallGridArray = fullGameArray[row]
-            console.log(smallGridArray)
-
-            for (let combos of winningCombos) {
-                let a = combos[0]; let b = combos[1]; let c = combos[2];
-                if (smallGridArray[a] !== '' && smallGridArray[a] == smallGridArray[b] && smallGridArray[a] == smallGridArray[c]) {
-                    for (i=0; i<smallGridArray.length; i++) {
-                        eventListenerTracker[row][i] = 2;
-                        console.log(eventListenerTracker[row]);
+        else {
+            if (nextLargeGrid === 'all') { // full grid
+                for (let row=0; row<9; row++) {
+                    for (let col=0; col<9; col++) {
+                        if (fullGameArray[row][col] === '') {
+                            fullGameEl[row][col].addEventListener('click', boxClicked);
+                            eventListenerTracker[row][col] = 'has event listener';
+                        }
                     }
-                    // largeGridEl[row].innerHTML = smallGridArray[a]
-                    console.log('winner')
-                    largeGameArray[row] = smallGridArray[a];
-                    console.log(largeGameArray)
-                    continue
+                }
+            }
+
+            else {
+                    for (let col=0; col<9; col++) { // specific grid
+                        if (fullGameArray[nextLargeGrid][col] === '') {
+                            fullGameEl[nextLargeGrid][col].addEventListener('click', boxClicked);
+                            eventListenerTracker[nextLargeGrid][col] = 'has event listener';
+                        }
+                    }
+            }
+        }
+    }
+
+    if (event === 'remove') {
+        for (let row=0; row<9; row++) {
+            for (let col=0; col<9; col++) {
+                if (eventListenerTracker[row][col] === 'has event listener') {
+                    fullGameEl[row][col].removeEventListener('click', boxClicked)
+                    eventListenerTracker[row][col] = 'no event listener'
                 }
             }
         }
     }
-    for (let combos of winningCombos) {
-        let a = combos[0]; let b = combos[1]; let c = combos[2];
-        if (largeGameArray[a] !== '' && largeGameArray[a] == largeGameArray[b] && largeGameArray[a] == largeGameArray[c]) {
-            gameWinner = largeGameArray[a];
-        }   
-    }   
 }
 
-function setUpBoardColours() {
-    for (let row=0; row<eventListenerTracker.length; row++) {
-        for (let col=0; col<eventListenerTracker.length; col++) {
-            if (eventListenerTracker[row][col] === 1) {
-                fullGameEl[row][col].style.backgroundColor = 'olive'
-            }
-            else if (eventListenerTracker[row][col] === -1) {
-                fullGameEl[row][col].style.backgroundColor = 'green'
-            }
-            else if (eventListenerTracker[row][col] === 2) {
-                if (fullGameArray[row][col] === 'X') {
-                    fullGameEl[row][col].style.backgroundColor = 'yellow'              
-                }
-                else if (fullGameArray[row][col] === 'O') {
-                    fullGameEl[row][col].style.backgroundColor = 'orange'
-                }
-                else {
-                    fullGameEl[row][col].style.backgroundColor = 'blue'
-                }
+function checkGame(largeGridPlayed) {
+    let winningCombos = [
+        [0,1,2],[3,4,5],[6,7,8],[0,3,4],
+        [1,4,7],[2,5,8],[0,4,8],[2,4,6],
+    ]
+
+    // check small grid and update large grid
+    row = fullGameArray[largeGridPlayed];
+    for (combo of winningCombos) {
+        let a = combo[0]; let b = combo[1]; let c = combo[2];
+        
+        if (row[a] !== '') {
+            if (row[a] === row[b] && row[a] === row[c]) {
+                largeGameArray[largeGridPlayed] = row[a];
+                largeGridTextEl[largeGridPlayed].innerHTML = row[a];
+                largeGridTextEl[largeGridPlayed].style.display = 'flex';
+                largeGridTextEl[largeGridPlayed].style.color = currentPlayerColour();
             }
         }
     }
+    // if a small grid is full and there is no winner, declare it a draw in the thing
+    if (countInstances(row, '') === 0 && largeGameArray[largeGridPlayed] === '') {
+        largeGameArray[largeGridPlayed] = '-';
+        largeGridTextEl[largeGridPlayed].innerHTML = '-';
+        largeGridTextEl[largeGridPlayed].style.display = 'flex';
+        largeGridTextEl[largeGridPlayed].style.color = 'black';
+    }
+ 
+    // check large grid
+    for (combo of winningCombos) {
+        let a = combo[0]; let b = combo[1]; let c = combo[2];
+        
+        if (largeGameArray[a] !== '') {
+            if (largeGameArray[a] === largeGameArray[b] && largeGameArray[a] === largeGameArray[c]) {
+                console.log(largeGameArray)
+            }
+        }
+    }
+
+    // if all large grids played and no winner, check points
+    if (countInstances(largeGameArray, '') === 0) {
+        let XPoints = countInstances(largeGameArray, 'X');
+        let OPoints = countInstances(largeGameArray, 'O');
+
+        if (XPoints < OPoints) {
+            console.log('Draw: O wins on poins')
+        }
+        else if (XPoints > OPoints) {
+            console.log('Draw: X wins on points')
+        }
+        else if (XPoints === OPoints) {
+            console.log('Draw')
+        }
+    }
 }
+
+function countInstances(array, target) {
+    const filteredArray = array.filter((element) => element === target);
+    return filteredArray.length;
+  }
 
 function changeCurrentPlayer() {
     if (currentPlayer === 'X') {
@@ -184,4 +191,26 @@ function changeCurrentPlayer() {
     else if (currentPlayer === 'O') {
         return 'X'
     }
+}
+
+function currentPlayerColour() {
+    if (currentPlayer === 'X') {
+        return XCol
+    }
+    else if (currentPlayer === 'O') {
+        return OCol
+    }
+}
+
+function nextLargeGridFunction(smallGridPlayed) {
+    if (largeGameArray[smallGridPlayed] === '') {
+        return smallGridPlayed
+    }
+    else {
+        return 'all'
+    }
+}
+
+function updateColours() {
+    
 }
